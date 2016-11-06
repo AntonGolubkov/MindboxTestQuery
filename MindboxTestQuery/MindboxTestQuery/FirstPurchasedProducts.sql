bf7869ed9c16d18c84eaff5545152ab9d65cf6f8
AntonTheFiremind <AntonTheFiremind@ATF-PC>
2016-11-06 20:13:58 +0300

    First commitment.

---
+++ MindboxTestQuery/MindboxTestQuery/FirstPurchasedProducts.sql
@@ -0,0 +1,8 @@
+﻿SELECT ProductId, COUNT(ProductId) AS Count
+FROM Sales INNER JOIN
+(
+	SELECT CustomerId, MIN(DateCreated) AS DateCreated
+	FROM Sales
+	GROUP BY CustomerId
+) AS FirstPurchases ON Sales.CustomerId = FirstPurchases.CustomerId AND Sales.DateCreated = FirstPurchases.DateCreated
+GROUP BY ProductId
